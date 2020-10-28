ec04083821cfc2b90a3cf319d2ce3790a29ae474
Milena Tselikina <milenochka.andreevna@mail.ru>
2020-10-28 18:09:44 +0400

    Add Postman project, fix process with payments

--- flink-streamlets/src/main/scala/payments/process/PaymentProcessingStreamlet.scala
+++ flink-streamlets/src/main/scala/payments/process/PaymentProcessingStreamlet.scala
@@ -3,8 +3,8 @@ package payments.process
 import cloudflow.flink._
 import cloudflow.streamlets.StreamletShape
 import cloudflow.streamlets.avro._
-import org.apache.flink.api.common.state.{ MapState, MapStateDescriptor }
-import org.apache.flink.streaming.api.functions.co.CoProcessFunction
+import org.apache.flink.api.common.state.{MapState, MapStateDescriptor}
+import org.apache.flink.streaming.api.functions.co.{CoProcessFunction, KeyedCoProcessFunction}
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.util.Collector
 import payments.datamodel._
@@ -22,21 +22,21 @@ class PaymentProcessingStreamlet extends FlinkStreamlet {
     override def buildExecutionGraph(): Unit = {
       val participants: DataStream[ParticipantInfo] =
         readStream(inParticipant)
-          .keyBy(_.participantId)
+          .keyBy(_ => "default")
 
       val payments: DataStream[PaymentObject] =
         readStream(inPayment)
-          .keyBy(_.receiverId)
+          .keyBy(_ => "default")
 
       val logInfo: DataStream[LogInfo] = participants
         .connect(payments)
-        .process(new MyProcess)
+        .process(new MyProcessFunction)
 
       writeStream(outLogger, logInfo)
     }
   }
 
-  class MyProcess extends CoProcessFunction[ParticipantInfo, PaymentObject, LogInfo] {
+  class MyProcessFunction extends KeyedCoProcessFunction[String, ParticipantInfo, PaymentObject, LogInfo] {
     @transient lazy val participantState: MapState[String, ParticipantInfo] =
       getRuntimeContext
         .getMapState(
@@ -46,25 +46,25 @@ class PaymentProcessingStreamlet extends FlinkStreamlet {
             classOf[ParticipantInfo]
           )
         )
+
     override def processElement1(
       value: ParticipantInfo,
-      ctx: CoProcessFunction[ParticipantInfo, PaymentObject, LogInfo]#Context,
+      ctx: KeyedCoProcessFunction[String, ParticipantInfo, PaymentObject, LogInfo]#Context,
       out: Collector[LogInfo]
-    ): Unit = participantState.put(value.participantId, value)
+    ): Unit = {
+      participantState.put(value.participantId, value)
+    }
 
     override def processElement2(
       value: PaymentObject,
-      ctx: CoProcessFunction[ParticipantInfo, PaymentObject, LogInfo]#Context,
+      ctx: KeyedCoProcessFunction[String, ParticipantInfo, PaymentObject, LogInfo]#Context,
       out: Collector[LogInfo]
     ): Unit = {
       value match {
         case PaymentObject(senderId, _, _) if !participantState.contains(senderId) =>
           out.collect(LogInfo("Sender does not exist.", "WARN"))
-          out.collect(LogInfo(s"$senderId", "WARN"))
         case PaymentObject(_, receiverId, _) if !participantState.contains(receiverId) =>
           out.collect(LogInfo("Receiver does not exist.", "WARN"))
-          out.collect(LogInfo(s"$receiverId", "WARN"))
-          out.collect(LogInfo(s"${participantState.keys()}", "WARN"))
         case PaymentObject(senderId, _, sum) if sum > participantState.get(senderId).balance =>
           out.collect(LogInfo("The sender has insufficient funds.", "WARN"))
         case PaymentObject(senderId, receiverId, sum) =>
